87fd525887d8eb9379f1836c05d3c52d69b5c21c
Shanosha <wootenm1@gmail.com>
2026-02-18 14:14:32 -0500

    feature/added example usage comments

--- src/App.tsx
+++ src/App.tsx
@@ -25,43 +25,51 @@ function App() {
 
   return (
     <>
-      <AlertBox type="success" message="This is a success alert!" onClose={() => alert('Alert closed!')}>
-        <p>Additional information about the success alert.</p>
-      </AlertBox>
 
-      <AlertBox type="warning" message="This is a warning alert!" onClose={() => alert('Alert closed!')}>
-        <p>Additional information about the warning alert.</p>
-      </AlertBox>
+      {/* Example usage of the AlertBox component. */}
 
-      <AlertBox type="error" message="This is a error alert!" onClose={() => alert('Alert closed!')}>
-        <p>Additional information about the error alert.</p>
-      </AlertBox>
+        <AlertBox type="success" message="This is a success alert!" onClose={() => alert('Alert closed!')}>
+          <p>Additional information about the success alert.</p>
+        </AlertBox>
+        
+        <AlertBox type="warning" message="This is a warning alert!" onClose={() => alert('Alert closed!')}>
+          <p>Additional information about the warning alert.</p>
+        </AlertBox>
+        
+        <AlertBox type="error" message="This is a error alert!" onClose={() => alert('Alert closed!')}>
+          <p>Additional information about the error alert.</p>
+        </AlertBox>
+        
+        <AlertBox type="info" message="This is a info alert!" onClose={() => alert('Alert closed!')}>
+          <p>Additional information about the info alert.</p>
+        </AlertBox>
 
-      <AlertBox type="info" message="This is a info alert!" onClose={() => alert('Alert closed!')}>
-        <p>Additional information about the info alert.</p>
-      </AlertBox>
+      {/* Example usage of the UserProfileCard component. */}
 
-      <UserProfileCard
-        user={user}
-        showEmail={true}
-        showRole={true}
-        onEdit={(userId) => alert(`Editing user ${userId}`)}
-      >
-        <div className="text-sm text-gray-500">
-          Last login: 2 hours ago
-        </div>
-      </UserProfileCard>
+        <UserProfileCard
+          user={user}
+          showEmail={true}
+          showRole={true}
+          onEdit={(userId) => alert(`Editing user ${userId}`)}
+        >
+          <div className="text-sm text-gray-500">
+            Last login: 2 hours ago
+          </div>
+        </UserProfileCard>
 
-      <ProductDisplay
-        product={product}
-        showDescription={true}
-        showStockStatus={true}
-        onAddToCart={(productId) => alert(`Added product ${productId} to cart`)}
-      >
-        <div className="text-sm text-gray-500">
-          Free shipping available
-        </div>
-      </ProductDisplay>
+      {/* Example usage of the ProductDisplay component. */}
+      
+        <ProductDisplay
+          product={product}
+          showDescription={true}
+          showStockStatus={true}
+          onAddToCart={(productId) => alert(`Added product ${productId} to cart`)}
+        >
+          <div className="text-sm text-gray-500">
+            Free shipping available
+          </div>
+        </ProductDisplay>
+
     </>
   )
 }
